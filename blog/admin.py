d51a93592ae07b02622802cd1420dd91d58f2852
SajalSisodia <sajalkumar801@gmail.com>
2023-11-26 16:28:21 +0530

    first commit

---
+++ blog/admin.py
@@ -0,0 +1,34 @@
+from django.contrib import admin
+from . models import *
+
+
+# Register your models here.
+
+
+@admin.register(PostHashTag)
+class PostHashTagadmin(admin.ModelAdmin):
+    list_display=["id","hashtag"
+                  
+    ]
+
+
+@admin.register(Blog)
+class Blogadmin(admin.ModelAdmin):
+    list_display=["id","title","date_time","description","summary",
+                  
+    ]
+
+
+
+@admin.register(Blog_Commnet)
+class Blog_Commnet_admin(admin.ModelAdmin):
+    list_display=["id","blog","comment","date_time"
+                  
+    ]
+
+
+@admin.register(Share_Blog)
+class Share_Blog_admin(admin.ModelAdmin):
+    list_display=["id","blog","token","email","date_time"
+                  
+    ]
