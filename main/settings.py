f043d8d2feffa26212bdc5cf5768051b89214916
SajalSisodia <sajalkumar801@gmail.com>
2023-12-10 10:54:04 +0530

    remove email

--- main/settings.py
+++ main/settings.py
@@ -124,7 +124,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = "static/"
 
 
@@ -139,7 +138,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'sajal89304@gmail.com'
-EMAIL_HOST_PASSWORD = 'lpgusqpwjmwlhbnl'
+EMAIL_HOST_PASSWORD = ''
 
 
 
